26b08793e07779777af188320fc03801fce223a4
Jean Amadeu <jean.amadeu2@gmail.com>
2022-07-27 19:40:41 -0300

    first commit

---
+++ src/main/kotlin/br/com/jamadeu/Webclient/WebclientApplication.kt
@@ -0,0 +1,11 @@
+package br.com.jamadeu.Webclient
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebclientApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebclientApplication>(*args)
+}
