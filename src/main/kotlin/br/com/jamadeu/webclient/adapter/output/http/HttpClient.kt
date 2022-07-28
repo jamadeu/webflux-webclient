641fe6149384a591507b83249f28d19dbd5f8d54
Jean Amadeu <jean.amadeu2@gmail.com>
2022-07-28 20:29:08 -0300

    error

--- src/main/kotlin/br/com/jamadeu/webclient/adapter/output/http/HttpClient.kt
+++ src/main/kotlin/br/com/jamadeu/webclient/adapter/output/http/HttpClient.kt
@@ -1,6 +1,5 @@
 package br.com.jamadeu.webclient.adapter.output.http
 
-import br.com.jamadeu.webclient.adapter.output.http.exception.ClientExceptionUtils
 import br.com.jamadeu.webclient.adapter.output.http.model.AddressByCepV2Response
 import br.com.jamadeu.webclient.configuration.HttpClientConfiguration
 import org.slf4j.LoggerFactory
@@ -9,6 +8,7 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
 import reactor.core.publisher.Mono
 import reactor.core.publisher.SignalType
 import java.util.logging.Level
@@ -17,27 +17,22 @@ import java.util.logging.Level
 class HttpClient(
     @Value("\${api.cep-v2}")
     private val uriCepV2: String,
-    private val httpClientConfiguration: HttpClientConfiguration,
-    private val clientExceptionUtils: ClientExceptionUtils
+    private val httpClientConfiguration: HttpClientConfiguration
 ) {
 
     private val logger = LoggerFactory.getLogger(javaClass)
 
     fun getAddressByCepV2(cep: String): Mono<AddressByCepV2Response> {
-        logger.info("Starting searching adrres, cep - $cep")
+        logger.info("Starting searching address, cep - $cep")
         return httpClientConfiguration.webClient()
             .get()
             .uri(uriCepV2, cep)
             .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
             .retrieve()
             .onStatus(HttpStatus::isError) {
-                clientExceptionUtils.exception(
-                    it.statusCode(),
-                    String.format("Address not found, cep - $cep")
-                )
+                Mono.error(ResponseStatusException(it.statusCode(), "Address not found"))
             }
             .bodyToMono(AddressByCepV2Response::class.java)
             .log("HttpClient.getAddressByCepV2", Level.INFO, SignalType.ON_COMPLETE)
-
     }
 }
--- src/main/kotlin/br/com/jamadeu/webclient/adapter/output/http/exception/ClientNotFoundException.kt
+++
@@ -1,3 +0,0 @@
-package br.com.jamadeu.webclient.adapter.output.http.exception
-
-class ClientNotFoundException(override val message: String, e: Throwable? = null) : RuntimeException(message, e)
